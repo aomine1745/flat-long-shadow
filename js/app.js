0921e2358c1f5406c130c125acf159fa822fa961
aomine <aomine1745@gmail.com>
2020-05-25 20:00:05 -0500

    v1.0

---
+++ js/app.js
@@ -0,0 +1,6 @@
+const text = document.getElementById('text')
+let shadow = ''
+for (let i = 0; i < 500; i++) {
+	shadow += (shadow ? ',' : '') + i * 1 + 'px ' + i * 1 + 'px 0 #01ded3'
+}
+text.style.textShadow = shadow
